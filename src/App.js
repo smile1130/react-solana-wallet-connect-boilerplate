431608db91c87e0295c8a3e18497a7010d1ca93d
smile1130 <superskiller1130@gmail.com>
2023-03-16 23:12:05 +0900

    Update App.js
    /* eslint-disable react/jsx-key */

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Router from './routes';
 import "./assets/scss/style.css"
 import { Wallets } from './components/wallet';
 
-// ----------------------------------------------------------------------
-
 export default function App() {
   return (
     <Wallets>
